87d16aa59c6489bb38ddf9e771b887059e71d431
filipelion <14293424+Filipelion@users.noreply.github.com>
2020-07-29 14:55:37 -0300

    rotas para SPA

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 
+import  {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+
+
+const Pagina404 = () => (<div>Página 404</div>) // Melhorar essa página de erro
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+       <Route path="/cadastro/video" component={CadastroVideo} />
+       <Route path="/cadastro/categoria" component={CadastroCategoria} />
+       <Route path="/" component={Home} exact />
+       <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
